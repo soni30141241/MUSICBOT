dc579ed1b0145999d9d9d16ade7e34524184653c
soni30141241 <soni30141241@gmail.com>
2026-02-12 09:30:12 +0530

    Update Youtube.py

--- AloneMusic/platforms/Youtube.py
+++ AloneMusic/platforms/Youtube.py
@@ -13,7 +13,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-from youtubesearchpython.__future__ import VideosSearch
+from ytSearch import VideosSearch, CustomSearch
 import base64
 from AloneMusic import LOGGER
 from AnonXMusic.utils.database import is_on_off
