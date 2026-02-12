d2ef5899cc02b501c2a74ad60ec97b18fafc0bc1
soni30141241 <soni30141241@gmail.com>
2026-02-12 09:16:06 +0530

    Update Youtube.py

--- AloneMusic/platforms/Youtube.py
+++ AloneMusic/platforms/Youtube.py
@@ -15,7 +15,7 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from ytSearch import VideosSearch, CustomSearch
 import base64
-from AnonXMusic import LOGGER
+from AloneMusic import LOGGER
 from AnonXMusic.utils.database import is_on_off
 from AnonXMusic.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
