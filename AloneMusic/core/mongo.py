eda19bf602ac53ea4c4cd1628c78019823c6fdd9
soni30141241 <soni30141241@gmail.com>
2026-06-13 07:35:30 +0530

    Add files via upload

---
+++ AloneMusic/core/mongo.py
@@ -0,0 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import MONGO_DB_URI
+
+from ..logging import LOGGER
+
+LOGGER(name).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.TGN
+    LOGGER(name).info("Connected to your Mongo Database.")
+except:
+    LOGGER(name).error("Failed to connect to your Mongo Database.")
+    exit()
